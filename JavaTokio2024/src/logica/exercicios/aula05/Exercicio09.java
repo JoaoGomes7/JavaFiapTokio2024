1374f55b57b54e1ab57c1fd86520d35a14127d6f
JoaoGomes7 <joaovictorsgomes07@gmail.com>
2024-09-01 13:02:40 -0300

    Quarto commit, exercicios da aula 5 de java concluidos.

--- JavaTokio2024/src/logica/exercicios/aula05/Exercicio09.java
+++ JavaTokio2024/src/logica/exercicios/aula05/Exercicio09.java
@@ -10,17 +10,17 @@ public class Exercicio09 {
 		System.out.println("escolha um numero de um a 999?");
 	    int num = Integer.parseInt(sc.nextLine());
 		
-		int centena = num / 100;
-		int centena2 = centena * 100;
 		
-		int dezena = num / 100;
-		int dezena2 = dezena * 10;
-		
-		int unidade = num / 100;
-		int unidade2 = unidade * 1;
-		
-		System.out.println("\n" + centena2 + "\n" + dezena2 + "\n"+unidade2);
-		 
+	    int centena = num / 100;
+	    int centena2 = centena * 100;
+	    
+	    int dezena = num % centena2;
+	    int dezena2 = dezena / 10;
+	    int dezena3 = dezena2 * 10;
+	    
+	    int unidade = dezena % dezena3;
+	    
+	    System.out.println("Centena: " + centena2 + "\nDezena: " + dezena3 + "\nUnidade: " + unidade);
 
 	}
 
--- JavaTokio2024/src/logica/exercicios/aula05/Exercicio10.java
+++ JavaTokio2024/src/logica/exercicios/aula05/Exercicio10.java
@@ -7,6 +7,22 @@ public class Exercicio10 {
 		
 		Scanner sc = new Scanner(System.in);
 		
+	    System.out.println("Quantos anos voce tem?");
+	    int anos = Integer.parseInt(sc.nextLine());
+	    
+	    System.out.println("Faz quantos meses completos que voce comemorou seu ultimo aniversario?");
+	    int meses = Integer.parseInt(sc.nextLine());
+	    
+	    System.out.println("Quantos dias se passaram apos o termino do mes passado?");
+	    int dias = Integer.parseInt(sc.nextLine());
+	    
+	    int DiasAnos = anos * 365;
+	    int DiasMeses = meses * 30;
+	    int DiasVividos = DiasAnos + DiasMeses;
+	    int DiasFinal = DiasVividos + dias;
+	    
+	    System.out.println("Voce viveu " + DiasFinal + " dias ate o momento.");
+	    
 
 	}
 
