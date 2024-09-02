ffa2741c8d5a599537c52675d0559c090d0cc1f1
joaoGomes7 <labsfiap@PA603MICRO20.fiap.com.br>
2024-09-02 16:46:36 -0300

    Atualizando os exercicios da aula 05

---
+++ JavaTokio2024/src/logica/exercicios/aula05/DesafioAula05.java
@@ -0,0 +1,31 @@
+package logica.exercicios.aula05;
+
+import java.util.Scanner;
+
+public class DesafioAula05 {
+
+	public static void main(String[] args) {
+		
+Scanner sc = new Scanner(System.in);
+		
+	    System.out.println("Quantos dias voce viveu na sua vida ate o momento?");
+	    int DiasVividos = Integer.parseInt(sc.nextLine());
+	    
+	   int anos = DiasVividos / 365;
+	    
+	   int DiasRAnos = 6357 - (anos * 365);
+	   
+	   int meses = DiasRAnos / 30;
+	   
+	   int dias = DiasRAnos - (meses * 30);
+	   
+	   
+	    System.out.println("Voce tem " + anos + " anos, " + meses + " meses e " + dias + " dias de vida." );
+	    
+	   
+	    
+		
+
+	}
+
+}
